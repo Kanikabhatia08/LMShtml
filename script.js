184a08c5d6e2b8eb0aaaae09fe5ca4460f3cc0c6
Kanika Bhatia <99886418+Kanikabhatia08@users.noreply.github.com>
2024-01-29 16:58:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+    let incrementOccurred = false;
+
+        function handleScroll() {
+        const scrollPosition = window.scrollY;
+        const counters = document.querySelectorAll('#counter');
+
+        if (scrollPosition >= 100 && !incrementOccurred) {
+            incrementOccurred = true;
+
+            counters.forEach((counterElement) => {
+            const maxValue = parseInt(counterElement.dataset.value, 10);
+            animateCounter(counterElement, maxValue);
+            });
+        }
+        }
+
+        function animateCounter(element, maxValue) {
+        let currentValue = 0;
+        const increment = 1; // Increment value
+
+        function updateCounter() {
+            if (currentValue <= maxValue) {
+            element.textContent = currentValue;
+            currentValue += increment;
+            requestAnimationFrame(updateCounter);
+            }
+        }
+
+        updateCounter();
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+
+    // mobile nav
+
+    const mobileMenuButton = document.getElementById('mobileMenuButton');
+    const mobileMenu = document.getElementById('mobileMenu');
+
+    mobileMenuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+    
