25fb8e6873678955a4813015708709e360648fba
Kanika <kanikabhatia08sep@gmail.com>
2024-01-29 17:47:09 +0530

    new changes

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+        fontFamily:{
+          
+            'exo': ["'Exo 2', sans-serif"],
+        },
+        colors: {
+            orange: '#FF782D',
+        }
+    }
+    },
+  plugins: [],
+}
+
